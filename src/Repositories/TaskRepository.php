50fd1f044577c9b99c0593ad63acafa859ad6729
Nguyen Van Quyen <quyen.nv@altplus.com.vn>
2016-02-16 11:45:13 -0800

    Update Task Reposiroty

--- src/Repositories/TaskRepository.php
+++ src/Repositories/TaskRepository.php
@@ -12,10 +12,31 @@ class TaskRepository
      * @param  User  $user
      * @return Collection
      */
-    public function getTaskByUserID($user)
+    public function getTasksByUserID($user)
     {
         return Task::where('user_id', $user->id)
                     ->orderBy('created_at', 'asc')
                     ->get();
+    }
+
+    /**
+     * create new Task 
+     * @param array $data
+     * @return mixed
+     */
+    public function create(array $data) {
+        $task = new Task;
+        $task->user_id = $data[0];
+        $task->name = $data[1];
+        return $task->save();
+    }
+
+    /**
+     * Destroy task by id of task
+     * @param $id
+     * @return mixed
+     */
+    public function deleteTaskByID($id) {
+        return Task::destroy($id);
     }
 }
